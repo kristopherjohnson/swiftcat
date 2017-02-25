b6d64e629c4313acc1fe36a593e06816dba92cc4
Kristopher Johnson <kris@kristopherjohnson.net>
2017-02-24 21:16:47 -0500

    Initial Commit

---
+++ swiftcat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftcat
+//
+//  Created by Kristopher Johnson on 2/24/17.
+//  Copyright © 2017 Kristopher Johnson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
